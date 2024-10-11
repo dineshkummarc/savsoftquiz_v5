5e756da664a22d13889a3564abba6a45010cece3
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-11 20:00:36 +0530

    updated

--- application/logs/log-2024-10-11.php
+++ application/logs/log-2024-10-11.php
@@ -3,3 +3,7 @@
 ERROR - 2024-10-11 19:12:53 --> Severity: Warning --> mysqli::__construct(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\wamp64\www\savsoftquiz_v5\application\config\config.php 515
 ERROR - 2024-10-11 19:25:08 --> Severity: Warning --> mysqli::__construct(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\wamp64\www\savsoftquiz_v5\application\config\config.php 515
 ERROR - 2024-10-11 19:37:05 --> Severity: Warning --> Division by zero C:\wamp64\www\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2024-10-11 19:56:45 --> Could not find the language line "hello"
+ERROR - 2024-10-11 19:56:45 --> Could not find the language line "user_id"
+ERROR - 2024-10-11 19:56:45 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\wamp64\www\savsoftquiz_v5\application\views\view_result.php 356
+ERROR - 2024-10-11 19:57:19 --> Could not find the language line "appointment_timing"
